0a51f98736bdca347f6d2aef011315956947ece6
Fazliddin <fazliddinazizkhojaev@gmail.com>
2021-02-07 20:56:19 +0500

    Add project files.

---
+++ Lesson 9/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Lesson_9
+{
+    static class ArrayHelper
+    {
+            public static T Pop<T>(ref T[] V)
+            {
+                T Temp = default;
+              
+                if (V.Length != 0)
+                {
+                    Temp = V[V.Length - 1];
+                    Array.Resize(ref V, V.Length - 1);
+                }
+                else
+                    Console.WriteLine("Array is empty ");
+                return Temp;
+
+            }
+            public static int Push<T>(ref T[] V, T x)
+            {
+                Array.Resize(ref V, V.Length + 1);
+                V[V.Length - 1] = x;
+                return V.Length;
+            }
+          
+            public static T Shift<T>(ref T[] V)
+            {
+                T Temp = default;
+                if (V.Length != 0)
+                {
+                    Array.Reverse(V);
+                    Temp = V[V.Length - 1];
+                    Array.Resize(ref V, V.Length - 1);
+                    Array.Reverse(V);
+                }
+                else
+                    Console.WriteLine("Array is empty");
+
+                return Temp;
+            }
+           
+            public static int UnShift<T>(ref T[] V, T x)
+            {
+                Array.Reverse(V);
+                Array.Resize(ref V, V.Length + 1);
+                V[V.Length - 1] = x;
+                Array.Reverse(V);
+                return V.Length;
+            }
+            public static T[] Slice<T>(ref T[] V)
+            {
+                T[] NewV = new T[V.Length];
+                for (int i = 0; i < V.Length; i++)
+                {
+                    NewV[i] = V[i];
+                }
+                return NewV;    
+            }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] V1 = { "aa", "bb", "cc", "dd" };
+            Console.WriteLine("Our Array ");
+            for (int i = 0; i < V1.Length; i++)
+                Console.Write(V1[i] + " ");
+            Console.WriteLine();
+            Console.WriteLine(ArrayHelper.Pop(ref V1));
+            Console.WriteLine();
+            Console.WriteLine("Our New Array ");
+            for (int i = 0; i < V1.Length; i++)
+                Console.Write(V1[i] + " ");
+        }
+    }
+}
